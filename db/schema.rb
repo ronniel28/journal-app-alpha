ab704534ec6d989117a5905b20077de27f8c59d5
Ronniel de Ramos <ronnielderamos@gmail.com>
2021-10-12 15:20:15 +0800

    adding user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_050516) do
+ActiveRecord::Schema.define(version: 2021_10_12_045701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,10 @@ ActiveRecord::Schema.define(version: 2021_10_11_050516) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "password_digest"
   end
 
   add_foreign_key "categories", "users"
