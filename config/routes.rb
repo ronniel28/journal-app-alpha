d62bb921937e785e2125d32adaf5c4e68a5d636e
Ronniel de Ramos <ronnielderamos@gmail.com>
2021-10-16 11:11:35 +0800

    add new task

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'sessions#new'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  resources :tasks
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
